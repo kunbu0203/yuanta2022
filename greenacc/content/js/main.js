ffa08306306211629dc61e60ab27a506baa155c9
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2022-06-24 11:30:41 +0800

    greenacc 0624

--- greenacc/content/js/main.js
+++ greenacc/content/js/main.js
@@ -1,13 +1,23 @@
 $(function () {
   // 進場
-  AOS.init({
-    offset: 200,
-    duration: 800,
-    once: true
-  }); // tool show/hide
+  $('[data-animate-block]').each(function () {
+    $(this).find('[data-animate]').each(function (index, el) {
+      var delay = 0.3 * index + 's';
 
+      if (index > 0) {
+        $(this).css('animation-delay', delay);
+      }
+    });
+  });
   $(window).on('scroll', function () {
-    var scrollTop = $(window).scrollTop();
+    var scrollTop = $(window).scrollTop(); // 進場
+
+    var halfScreenH = $(window).height() / 1.5;
+    $('[data-animate-block]').each(function (index, el) {
+      if (scrollTop > $(el).offset().top - halfScreenH) {
+        $(el).attr('data-animate-block', 'active');
+      }
+    }); // tool show/hide
 
     if (scrollTop > 200) {
       $('.mediaIcons').fadeIn(300);
@@ -19,6 +29,17 @@ $(function () {
       $('.gotop').fadeIn(300);
     } else {
       $('.gotop').fadeOut(300);
+    } // tab
+
+
+    var headerTop = $('[data-header]').offset().top + $('[data-header]').outerHeight();
+    var tabTop = $('[data-nav]').offset().top,
+        $fixed = $('[data-fixed]');
+
+    if (headerTop > tabTop) {
+      $fixed.addClass('-fixed');
+    } else {
+      $fixed.removeClass('-fixed');
     }
   }).trigger('scroll'); // goTop鈕
 
@@ -57,19 +78,24 @@ $(function () {
     $('[data-tab], [data-tab-content]').removeClass('-active');
     $(this).addClass('-active');
     $('[data-tab-content="' + val + '"]').addClass('-active');
-    AOS.refreshHard();
+    $('html, body').scrollTop($('[data-nav]').offset().top);
+    $('[data-animate-block]').attr('data-animate-block', '');
+    $(window).trigger('scroll');
   });
-  $(window).on('scroll', function () {
-    var scrollTop = $('[data-header]').offset().top + $('[data-header]').outerHeight(),
-        comTop = $('[data-nav]').offset().top,
-        $fixed = $('[data-fixed]');
 
-    if (scrollTop > comTop) {
-      $fixed.addClass('-fixed');
-    } else {
-      $fixed.removeClass('-fixed');
-    }
-  }).trigger('scroll'); // bar
+  if (location.href.indexOf('?') != -1) {
+    var paramAry = location.href.split('?')[1].split('&'),
+        tabStr = paramAry.find(function (item, index) {
+      return item.indexOf('tab') != -1;
+    });
+    var name = tabStr.split('=')[1];
+    $('[data-tab], [data-tab-content]').removeClass('-active');
+    $('[data-tab="' + name + '"]').addClass('-active');
+    $('[data-tab-content="' + name + '"]').addClass('-active');
+    $('html, body').scrollTop($('[data-nav]').offset().top);
+    $(window).trigger('scroll');
+  } // bar
+
 
   var bar = document.querySelector('[data-bar-main]'),
       barMb = document.querySelector('[data-bar-main-mb]');
@@ -123,5 +149,14 @@ $(function () {
   barMb.noUiSlider.on('update', function (val) {
     var val = parseInt(val);
     $('[data-bar-mb]').removeClass().addClass('bar -mb is-' + val);
+  });
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  $('[data-login]').on('click', function (e) {
+    if (!isMobile) {
+      e.preventDefault();
+      window.open('https://www.yuantabank.com.tw/bankwebIMG/event/Bank_Act2019/MobileBankingAPP/index.html#/1', '_blank');
+    } else {
+      return;
+    }
   });
 });
